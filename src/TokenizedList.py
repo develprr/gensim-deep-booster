991efe0bea2fabd8f68b9f75c2876e85e45b7ae7
Heikki Kupiainen <heikki.kupiainen@qatalis.fi>
2023-06-01 13:02:33 +0300

    Wrote TokenizedList module to convert phrase lists to tokenized lists

---
+++ src/TokenizedList.py
@@ -0,0 +1,16 @@
+# made by Heikki Kupiainen 2023
+
+# TokenizedList module provides tools to convert phrase lists
+# into tokenized lists
+
+import gensim
+import PhraseList
+from gensim import corpora
+from gensim.utils import simple_preprocess
+
+def build_from_phrase_list(phrase_list: list[str]):
+  return [simple_preprocess(doc) for doc in phrase_list]
+
+def build_from_sample_phrase_list():
+  phrase_list = PhraseList.get_sample_phrases()
+  return build_from_phrase_list(phrase_list)  

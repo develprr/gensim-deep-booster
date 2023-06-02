32267dfc5ecab87bcf42e42dfb9042536e135d99
Heikki Kupiainen <heikki.kupiainen@qatalis.fi>
2023-06-02 10:56:11 +0300

    Created BWCorpus class

---
+++ src/BWCorpus.py
@@ -0,0 +1,42 @@
+# Made by Heikki Kupiainen 2023
+
+import gensim
+from gensim import corpora
+from gensim.utils import simple_preprocess
+from smart_open import smart_open
+import nltk
+import os
+from dotenv import load_dotenv
+
+nltk.download('stopwords')  # run once
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+
+import TokenizedList
+import PhraseList
+
+load_dotenv()
+
+SAMPLE_TEXT_SOURCE_FILE = os.getenv('SAMPLE_TEXT_SOURCE_FILE')    
+CORPUS_SERIALIZATION_DIR = os.getenv('CORPUS_SERIALIZATION_DIR')
+
+class BWCorpus(object):
+
+  def __init__(self, dictionary, corpus):
+    self.dictionary = dictionary
+    self.corpus = corpus
+    
+  def __repr__(self):
+      return f"<BowCorpus corpus:{self.corpus} dictionary:{self.dictionary} >"
+      
+  @staticmethod
+  def build_from_phrase_list(phrase_list: list[str]):
+    tokenized_list = TokenizedList.build_from_phrase_list(phrase_list)
+    dictionary = corpora.Dictionary()
+    corpus = [dictionary.doc2bow(doc, allow_update=True) for doc in tokenized_list]
+    return BWCorpus(dictionary, corpus)
+  
+  @staticmethod
+  def build_from_sample_phrase_list():
+    phrase_list = PhraseList.get_sample_phrases()
+    return BWCorpus.build_from_phrase_list(phrase_list)

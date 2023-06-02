b8c59f517641fcd7b663d226dbb00e1f791fde86
Heikki Kupiainen <heikki.kupiainen@qatalis.fi>
2023-06-02 08:35:17 +0300

    BowCorpus: added logic to load corpus from a file

--- src/BowCorpus.py
+++ src/BowCorpus.py
@@ -3,9 +3,21 @@
 import gensim
 from gensim import corpora
 from gensim.utils import simple_preprocess
+from smart_open import smart_open
+import nltk
+import os
+from dotenv import load_dotenv
+
+nltk.download('stopwords')  # run once
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
 
 import TokenizedList
 import PhraseList
+
+load_dotenv()
+
+SAMPLE_TEXT_SOURCE_FILE = os.getenv('SAMPLE_TEXT_SOURCE_FILE')    
 
 class BowCorpus:
   def __init__(self, dictionary, corpus):
@@ -34,3 +46,18 @@ def count_words_in_sample_corpus():
   bow_corpus = build_from_sample_phrase_list()
   word_counts = get_word_counts(bow_corpus)
   print(word_counts)
+
+def build_from_file(filepath: str):
+  corpus = []
+  dictionary = corpora.Dictionary()
+  for line in smart_open(filepath, encoding='utf8'):
+    tokenized_list = simple_preprocess(line, deacc=True)
+    bow = dictionary.doc2bow(tokenized_list, allow_update=True)
+    corpus.append(bow)
+  return BowCorpus(dictionary, corpus)
+
+def build_from_sample_file():
+  print(SAMPLE_TEXT_SOURCE_FILE)
+
+  corpus = build_from_file(SAMPLE_TEXT_SOURCE_FILE)
+  print(corpus)

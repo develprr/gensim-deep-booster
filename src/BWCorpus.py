fd61b5b5bc4204fa23afa4712e52518642759485
Heikki Kupiainen <heikki.kupiainen@qatalis.fi>
2023-06-02 15:06:52 +0300

    Changed BWCorpus to rely on module methods, minimizing the actual class. Wrote tests.

--- src/BWCorpus.py
+++ src/BWCorpus.py
@@ -27,16 +27,28 @@ class BWCorpus(object):
     self.corpus = corpus
     
   def __repr__(self):
-      return f"<BowCorpus corpus:{self.corpus} dictionary:{self.dictionary} >"
-      
-  @staticmethod
-  def build_from_phrase_list(phrase_list: list[str]):
-    tokenized_list = TokenizedList.build_from_phrase_list(phrase_list)
-    dictionary = corpora.Dictionary()
-    corpus = [dictionary.doc2bow(doc, allow_update=True) for doc in tokenized_list]
-    return BWCorpus(dictionary, corpus)
-  
-  @staticmethod
-  def build_from_sample_phrase_list():
-    phrase_list = PhraseList.get_sample_phrases()
-    return BWCorpus.build_from_phrase_list(phrase_list)
+      return f"<BWCorpus corpus:{self.corpus} dictionary:{self.dictionary} >"
+    
+def get_dictionary(corpus: BWCorpus):
+  return corpus.dictionary
+
+def get_corpus(corpus: BWCorpus):
+  return corpus.corpus
+
+def build_from_sample_phrase_list() -> BWCorpus:
+  phrase_list = PhraseList.get_sample_phrases()
+  return build_from_phrase_list(phrase_list)
+
+def build_from_phrase_list(phrase_list: list[str]) -> BWCorpus:
+  tokenized_list = TokenizedList.build_from_phrase_list(phrase_list)
+  dictionary = corpora.Dictionary()
+  corpus = [dictionary.doc2bow(doc, allow_update=True) for doc in tokenized_list]
+  return BWCorpus(dictionary, corpus)
+
+def test_build_from_phrase_list():
+  bw_corpus = build_from_sample_phrase_list()
+  assert(type(bw_corpus)) == BWCorpus
+  dictionary = get_dictionary(bw_corpus)
+  assert(type(dictionary)) == corpora.Dictionary
+  corpus = get_corpus(bw_corpus)
+  assert(type(corpus)) == list

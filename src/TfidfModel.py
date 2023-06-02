fecbbdb6fff3e9aa92a561c998886bf0afebc876
Heikki Kupiainen <heikki.kupiainen@qatalis.fi>
2023-06-02 15:40:33 +0300

    Made class object itself to be minimalistic, while module itself is more class-like

--- src/TfidfModel.py
+++ src/TfidfModel.py
@@ -1,27 +1,52 @@
 # Made by Heikki Kupiainen 2023
 
-from BWCorpus import BWCorpus
+import BWCorpus
+
 from gensim import models
+from gensim import corpora
+
 import numpy as np
 
 class TfidfModel(object):
   
-  def __init__(self,corpus):
-    self.corpus = corpus
-    self.model = models.TfidfModel(corpus, smartirs='ntc')
+  def __init__(self,bw_corpus: BWCorpus.Model):
+    self.corpus = bw_corpus
+    self.model = models.TfidfModel(bw_corpus.corpus, smartirs='ntc')
 
-  @staticmethod
-  def build_from_bw_corpus(bw_corpus: BWCorpus):
-    corpus = bw_corpus.corpus
-    return TfidfModel(corpus)
-  
+    
+  def print(self):
+    for doc in self.corpus:
+      print([[[id], np.around(freq, decimals=2)] for id, freq in doc])
+      
+def build_from_bw_corpus(bw_corpus: BWCorpus.Model):
+  return TfidfModel(bw_corpus)
+
+
+def get_dictionary(model: TfidfModel):
+    corpus = model.corpus
+    return BWCorpus.get_dictionary(corpus);
+    
+
+def build_sample() -> TfidfModel:
+  bw_corpus = BWCorpus.build_from_sample_phrase_list()
+  return build_from_bw_corpus(bw_corpus)
+
+def test_build_sample():
+  model = build_sample()
+  assert(type(model)) == TfidfModel
+
+def test_get_dictionary():
+  model = build_sample()
+  dictionary = get_dictionary(model) 
+  assert(type(dictionary)) == corpora.Dictionary
+
 def test_build_corpus():
   bw_corpus = BWCorpus.build_from_sample_phrase_list()
   print(type(bw_corpus.corpus))
   
-  assert(type(bw_corpus)) == BWCorpus
-  tfidf_model = TfidfModel.build_from_bw_corpus(bw_corpus)
+  assert(type(bw_corpus)) == BWCorpus.Model
+  tfidf_model = build_from_bw_corpus(bw_corpus)
   
   assert(type(tfidf_model)) == TfidfModel
-  assert(type(tfidf_model.corpus)) == list
+  assert(type(tfidf_model.corpus)) == BWCorpus.Model
   assert(type(tfidf_model.model)) == models.tfidfmodel.TfidfModel

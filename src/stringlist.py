f8c1c584ae4f9733bc6b04042e11dd7d8940c0ad
Heikki Kupiainen <heikki.kupiainen@qatalis.fi>
2023-06-04 12:53:48 +0300

    Simplified StringList instantiations with a static build method.

--- src/stringlist.py
+++ src/stringlist.py
@@ -5,8 +5,13 @@ from pydantic import BaseModel, StrictStr
 
 class StringList(BaseModel):
   items: List[StrictStr]
+  
+  @staticmethod
+  def build(strings: list[str]):
+    return StringList(**{
+      'items': ['list', 'of', 'words']
+    })
+
 
 def test_instantiation():
-  assert(type((StringL(**{
-    'items': ['list', 'of', 'words']
-  })))) == StringList
+  assert(type(StringList.build(['list', 'of', 'words']))) == StringList

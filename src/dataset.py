0bfcc36ba1d2021d7fe3c30da846255a9be09e2f
Heikki Kupiainen <heikki.kupiainen@qatalis.fi>
2023-06-04 12:33:09 +0300

    Enhanced StringDataSet with an ability to be instantiated from text8

--- src/dataset.py
+++ src/dataset.py
@@ -4,9 +4,21 @@ from datetime import datetime
 from typing import List, Optional
 from pydantic import BaseModel, StrictStr, StrictInt
 
+from api import Api
 class StringDataset(BaseModel):
   items: List[List[StrictStr]]
+  
+  @staticmethod
+  def build_from_text8():
+    items = Api.load_text8_dataset()
+    return StringDataset(**{
+      'items': items
+    })
 
+def test_build_from_text8():
+  dataset = StringDataset.build_from_text8()
+  assert(type(dataset)) == StringDataset
+  
 def test_instantiation():
   assert(type((StringDataset(**{
     'items': [

2f8f78f631a858a35f57af49ede519201d472063
Heikki Kupiainen <heikki.kupiainen@qatalis.fi>
2023-06-04 09:35:04 +0300

    Wrote api module to find similar words using gensim library.

---
+++ src/api.py
@@ -0,0 +1,28 @@
+# (C) by Heikki Kupiainen 2023    
+
+import gensim.downloader as downloader
+
+def info_gigaworld():
+  print(downloader.info('glove-wiki-gigaword-50'))
+
+# Finds most similar items to given example:
+def find_similar(word: str):
+  w2v_model = downloader.load("glove-wiki-gigaword-50")
+  return w2v_model.most_similar(word.lower())
+
+def find_similar_to_jupiter():
+  return find_similar("Jupiter")
+  
+def test_find_similar():
+  print("blue...")
+  answers = find_similar("blue")
+  assert(answers[0][0]) == "red"
+  print("king...")
+  answers = find_similar("king")
+  assert(answers[0][0]) == "prince"
+  print("sweden...")
+  answers = find_similar("sweden")
+  assert(answers[0][0]) == "denmark"
+  print("estonia...")
+  answers = find_similar("estonia")
+  assert(answers[0][0]) == "latvia"

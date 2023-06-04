9a84a11ec58ad42c1e574390247f0b4ea24d2765
Heikki Kupiainen <heikki.kupiainen@qatalis.fi>
2023-06-04 08:48:21 +0300

    Refactored TfIdf module to be more idiomatic to Python language conventions by changing instance related module functions to true instance functions.

--- src/tfidf.py
+++ src/tfidf.py
@@ -16,55 +16,53 @@ class TfIdf(object):
   def __repr__(self):
       return f"<TfiIdf bw_corpus:{self.bw_corpus} model:{self.model} >"
   
-  def print(self):
-    for doc in self.corpus:
-      print([[[id], np.around(freq, decimals=2)] for id, freq in doc])
-      
-      
-def build_from_bw_corpus(bw_corpus: BWCorpus.Model):
-  return TfIdf(bw_corpus)
-
-def get_dictionary(model: TfIdf):
-  corpus = model.bw_corpus
-  return BWCorpus.get_dictionary(corpus);
-    
-def get_bw_corpus(tf_idf: TfIdf):
-  return tf_idf.bw_corpus
+  def get_bw_corpus(self):
+    return self.bw_corpus
   
-def get_corpora_corpus(tf_idf: TfIdf):
-    bw_corpus = get_bw_corpus(tf_idf)
+  def get_corpora_corpus(self):
+    bw_corpus = self.get_bw_corpus()
     return BWCorpus.get_corpora_corpus(bw_corpus)
+
+  def get_dictionary(self):
+    return BWCorpus.get_dictionary(self.bw_corpus);    
+  
+  def print(self):
+    for doc in self.get_corpora_corpus():
+      print([[[id], np.around(freq, decimals=2)] for id, freq in doc])
     
 def build_sample() -> TfIdf:
   bw_corpus = BWCorpus.build_sample()
-  return build_from_bw_corpus(bw_corpus)
-
+  return TfIdf(bw_corpus)
 
 def test_build_sample():
   model = build_sample()
   assert(type(model)) == TfIdf
 
 def test_get_dictionary():
-  model = build_sample()
-  dictionary = get_dictionary(model) 
+  tf_idf = build_sample()
+  dictionary = tf_idf.get_dictionary() 
   assert(type(dictionary)) == corpora.Dictionary
 
 def test_get_bw_corpus():
   tf_idf = build_sample()
   print(tf_idf)
-  bw_corpus = get_bw_corpus(tf_idf)
+  bw_corpus = tf_idf.get_bw_corpus()
   assert(type(bw_corpus)) == BWCorpus.Model
   
+def test_print():
+  tf_idf = build_sample()
+  tf_idf.print()
+
 def test_get_corpora_corpus():
   tf_idf = build_sample()
-  ccorpus = get_corpora_corpus(tf_idf)
+  ccorpus = tf_idf.get_corpora_corpus()
   assert(type(ccorpus)) == list
 
 def test_build_corpus():
   bw_corpus = BWCorpus.build_sample()
   print(type(bw_corpus.corpus))
   assert(type(bw_corpus)) == BWCorpus.Model
-  tfidf_model = build_from_bw_corpus(bw_corpus)
+  tfidf_model = TfIdf(bw_corpus)
   assert(type(tfidf_model)) == TfIdf
   assert(type(tfidf_model.bw_corpus)) == BWCorpus.Model
   assert(type(tfidf_model.model)) == models.tfidfmodel.TfidfModel
